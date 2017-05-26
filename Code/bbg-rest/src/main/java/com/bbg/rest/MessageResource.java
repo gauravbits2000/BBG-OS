37b61ef47d998215df3591d9ee01a95e307006d1
Gaurav.Agarwal <Gaurav.Agarwal@gmail.com>
2017-05-26 16:02:24 +0530

    .

--- Code/bbg-rest/src/main/java/com/bbg/rest/MessageResource.java
+++ Code/bbg-rest/src/main/java/com/bbg/rest/MessageResource.java
@@ -10,13 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriInfo;
 
 import com.bbg.exceptions.DataNotFoundException;
 import com.bbg.model.Message;
